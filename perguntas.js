efe44a8a1ad0fb03994435d9933bd9a7f72ea3a3
Corrupt-org <oliveira.santos.matheus0604@escola.pr.gov.br>
2025-09-18 08:04:37 -0300

    JAVASCRIPT ADICIONADO LOL

---
+++ perguntas.js
@@ -0,0 +1,12 @@
+fazCard (
+    "Arte"
+    "Quem pintou A Mona Lisa?"
+    "Leonardo da Vinci foi o mestre renascentista que pintou A Mona Lisa"
+)
+
+fazCard (
+    "Arte"
+    "Qual a função da arte?"
+    "A arte tem funções como estética (prazer visual), expressiva, simbólica (comunicação) e crítica"
+)
+
