f645d676dd2f2cb27d2c8c3e82c4bc5d419a1e74
Corrupt-org <oliveira.santos.matheus0604@escola.pr.gov.br>
2025-09-23 08:02:00 -0300

    UPDATES js

--- perguntas.js
+++ perguntas.js
@@ -1,12 +1,17 @@
-fazCard (
+fazCard(
     "Arte"
     "Quem pintou A Mona Lisa?"
     "Leonardo da Vinci foi o mestre renascentista que pintou A Mona Lisa"
 )
 
-fazCard (
+fazCard(
     "Arte"
     "Qual a função da arte?"
     "A arte tem funções como estética (prazer visual), expressiva, simbólica (comunicação) e crítica"
 )
 
+fazCard(
+    "Arte"
+    "Qual a nacionalidade de William Shakespeare?"
+    "Inglesa, tendo nascido e falecido em Stratford-upon-Avon, Inglaterra."
+)
